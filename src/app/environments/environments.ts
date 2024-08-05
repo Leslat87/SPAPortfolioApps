a47804ed8da4efa8161afa2c574ad15a98fa932e
Leslat87 <130540174+Leslat87@users.noreply.github.com>
2024-08-05 12:36:28 +0200

    modified:   src/app/environments/environments.ts

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,4 +1,8 @@
-// src/environments/environments.ts
+declare var process: {
+  env: {
+    API_BASE_URL: string;
+  }
+};
 export const environment = {
   production: false,
   baseUrl: 'http://localhost:3000'
